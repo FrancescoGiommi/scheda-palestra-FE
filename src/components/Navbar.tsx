5c329f33f274f48c57b41bfe846b54a988434403
Francesco <francescogiommi97@icloud.com>
2025-10-07 22:57:43 +0200

    Add fix Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,16 @@ export default function Navbar() {
     <>
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand">GymFit</a>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
+          <a className="navbar-brand" href="#">
+            <img
+              src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+              alt="Logo"
+              width="30"
+              height="24"
+              className="d-inline-block align-text-top"
             />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+            GymFit
+          </a>
         </div>
       </nav>
     </>
