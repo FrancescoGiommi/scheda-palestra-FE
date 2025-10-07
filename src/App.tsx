46697b38264fa1f5af8a82855680a6cb121cf542
Francesco <francescogiommi97@icloud.com>
2025-10-07 22:56:20 +0200

    Add Navbar, Dock and style css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import Dock from "./components/Dock";
 
 import "./App.css";
 
@@ -8,9 +10,11 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
         </Routes>
+        <Dock />
       </BrowserRouter>
     </>
   );
---
+++ src/components/Dock.tsx
@@ -0,0 +1,20 @@
+export default function Dock() {
+  return (
+    <>
+      <nav className="dock-nav">
+        <a href="#">
+          <img src="icon-home.svg" alt="Home" />
+        </a>
+        <a href="#">
+          <img src="icon-list.svg" alt="Schede" />
+        </a>
+        <a href="#">
+          <img src="icon-plus.svg" alt="Nuova" />
+        </a>
+        <a href="#">
+          <img src="icon-user.svg" alt="Profilo" />
+        </a>
+      </nav>
+    </>
+  );
+}
